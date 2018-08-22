d4adf98abc215da2105973de292d10a6aec3e448
7upesi <7upesi@gmail.com>
2018-08-22 13:07:34 -0400

    It almost works

--- index.py
+++ index.py
@@ -13,28 +13,32 @@ async def on_ready():
     print('------')
 
 @bot.command()
+async def thetruth(ctx):
+    await ctx.send("traps are gay!")
+
+@bot.command()
 async def roll(ctx, dice: str):
     diceNum = 0
     dieNum = 0
     endRoll = 0
     parameter1 = dice.split("d", 1)
-    diceNum = parameter1.pop(0)
+    diceNum = int(parameter1.pop(0))
     parameter2 = parameter1.pop(0)
     if parameter2.isdigit():
-        dieNum = parameter2
+        dieNum = int(parameter2)
         for _ in range(diceNum):
-            endRoll += rand.randInt(1, dieNum + 1)
+            endRoll += random.randint(1, dieNum + 1)
     else:
         parameter3 = parameter2.split("-", 1)
-        dieNum = parameter3.pop(0)
+        dieNum = int(parameter3.pop(0))
         if(parameter3.len == 1):
-            addNum = parameter3.pop(0)
+            addNum = int(parameter3.pop(0))
             for _ in range(diceNum):
-                endRoll += rand.randInt(1, dieNum + 1) + addNum
+                endRoll += random.randint(1, dieNum + 1) + addNum
         else:
-            subNum = parameter3.pop(0)
+            subNum = int(parameter3.pop(0))
             for _ in range(diceNum):
-                endRoll += rand.randInt(1, dieNum + 1) - subNum
+                endRoll += random.randint(1, dieNum + 1) - subNum
     await ctx.send(endRoll)
 
 bot.run(config.token)
